7f13022694ecc3008ad2d6a271388944844c4eed
Renner Borges <rennerferreira23@gmail.com>
2022-09-21 20:43:10 -0300

    feat: exercicio 15

---
+++ Exercicios 17-08-2022/Atividade 15/ola-mun arqs/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter">
+      <formulario />
+      <TitleComponent value="Sou um componente do Vue.js" />
+    </div>
+  </main>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import Formulario from './components/Formulario.vue';
+import TitleComponent from './components/TitleComponent.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral,
+    Formulario,
+    TitleComponent,
+  },
+});
+</script>
+
+<style></style>
---
+++ Exercicios 17-08-2022/Atividade 15/ola-mun arqs/src/components/TitleComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>{{ value }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TitleComponent',
+  props: {
+    value: String,
+  },
+});
+</script>
+
+<style scoped>
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
